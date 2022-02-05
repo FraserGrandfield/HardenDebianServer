6fde237b1cb322dbf0d4eb381e37fdaaa5ce4236
FraserGrandfield <frasergrandfield110@gmail.com>
2022-02-05 21:17:33 +0000

    Added colours to mesages

--- src/hardenServer.py
+++ src/hardenServer.py
@@ -2,39 +2,52 @@ import argparse
 import subprocess
 from tabnanny import check
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def output_info(process, output):
-    if check_error(process):
-        if (args.verbose):
-            print(process.stdout.strip().decode('ascii'))
-        else:
-            print(output)
-    else:
+    if is_error(process):
         if (args.verbose):
             print(process.stderr.strip().decode('ascii'))
         else:
-            print("An error has occured, use [-v] to see more info")
-
-def check_error(process):
-    if (process.returncode > 0):
-        return False
+            print(bcolors.WARNING + "An error has occured, use [-v] to see more info" + bcolors.ENDC)
     else:
+        if (args.verbose):
+            print(process.stdout.strip().decode('ascii'))
+            print(bcolors.OKGREEN + output + bcolors.ENDC)
+        else:
+            print(bcolors.OKGREEN + output + bcolors.ENDC)
+
+def is_error(process):
+    if (process.returncode > 0):
         return True
+    else:
+        return False
 
 #TEST print ls
 def print_ls():
+    print(bcolors.OKBLUE + "Running 'ls'" + bcolors.ENDC)
     completedProcess = subprocess.run(["ls"], capture_output=True)
-    print(completedProcess.stdout.strip().decode('ascii'))
+    output_info(completedProcess, "ran 'ls'")
 
 #TEST ipconfig
 def print_ip():
-    print("Running 'ipconfig'")
+    print(bcolors.OKBLUE + "Running 'ipconfig'" + bcolors.ENDC)
     completedProcess = subprocess.run(["ipconfig"], capture_output=True)
-    check_error(completedProcess)
     output_info(completedProcess, "ran 'ipconfig'")
 
 def update_upgrade():
+    print(bcolors.OKBLUE + "Starting update" + bcolors.ENDC)
     completedProcess = subprocess.run(["sudo", "apt-get", "update"], capture_output=True)
-    print(completedProcess)
+    output_info(completedProcess, "Update complete")
 
 if __name__ == "__main__":
     #Parser to get flags.
@@ -45,7 +58,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     #TESTS
-    #print_ip()
+    print_ip()
     #print_ls()
     
     #Update and upgrade packeges
