349e330ea5f60f864742ad4bebd41e6344becd15
FraserGrandfield <45278231+FraserGrandfield@users.noreply.github.com>
2022-02-05 18:36:00 +0000

    Added test processes

--- src/hardenServer.py
+++ src/hardenServer.py
@@ -1,11 +1,25 @@
 import argparse
 import subprocess
 
+#TEST print ls
+def print_ls():
+    completedProcess = subprocess.run(["ls"], capture_output=True)
+    print(completedProcess.stdout.strip().decode('ascii'))
+
+#TEST ipconfig
+def print_tree():
+    completedProcess = subprocess.run(["ipconfig"], capture_output=True)
+    print(completedProcess.stdout.strip().decode('ascii'))
+
 if __name__ == "__main__":
     #Parser to get flags.
     parser = argparse.ArgumentParser(description="Harden Debian server.")
     parser.add_argument("-v", "--verbose", help="Print verbose", dest="verbose", action="store_true")
     parser.add_argument("-q", "--qq", help="Print verbose", dest="yeet", action="store_true")
-    args = parser.parse_args()    
-
-    print("Hello World")
+    args = parser.parse_args()
+    
+    #TESTS
+    #print_tree()
+    #print_ls()
+    
+    #Update and upgrade packeges
